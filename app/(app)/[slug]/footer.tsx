5e4dd25462f89a9d6fa56d4a35152ad967cf85a1
Duong Nguyen <dnguyenlearning@gmail.com>
2024-07-10 11:40:53 +0700

    test navigate

--- app/(app)/[slug]/footer.tsx
+++ app/(app)/[slug]/footer.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@/components/ui/button";
 import {
   CalendarIcon,
+  HeartIcon,
   HistoryIcon,
   SearchIcon,
   UserRoundIcon,
@@ -8,20 +9,28 @@ import {
 
 export function Footer() {
   return (
-    <div className="flex justify-center gap-8 px-4 py-2 shadow">
-      <Button size="icon" variant={"default"}>
+    <div className="flex justify-center gap-8 px-4 py-3 rounded-t-3xl shadow">
+      <Button
+        size="icon"
+        className="w-12 h-12 rounded-full"
+        variant={"default"}
+      >
         <CalendarIcon className="w-6 h-6" />
       </Button>
 
-      <Button size="icon" variant={"ghost"}>
+      <Button size="icon" className="w-12 h-12 rounded-full" variant={"ghost"}>
         <SearchIcon className="w-6 h-6" />
       </Button>
 
-      <Button size="icon" variant={"ghost"}>
+      <Button size="icon" className="w-12 h-12 rounded-full" variant={"ghost"}>
         <HistoryIcon className="w-6 h-6" />
       </Button>
 
-      <Button size="icon" variant={"ghost"}>
+      <Button size="icon" className="w-12 h-12 rounded-full" variant={"ghost"}>
+        <HeartIcon className="w-6 h-6" />
+      </Button>
+
+      <Button size="icon" className="w-12 h-12 rounded-full" variant={"ghost"}>
         <UserRoundIcon className="w-6 h-6" />
       </Button>
     </div>
