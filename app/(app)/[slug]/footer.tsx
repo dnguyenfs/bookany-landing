60d94d8643b99f453e4758a24227deb01cd1fef1
Duong Nguyen <dnguyenlearning@gmail.com>
2024-07-19 12:19:58 +0700

    update

--- app/(app)/[slug]/footer.tsx
+++ app/(app)/[slug]/footer.tsx
@@ -9,28 +9,44 @@ import {
 
 export function Footer() {
   return (
-    <div className="flex justify-center gap-8 px-4 py-3 rounded-t-3xl shadow">
+    <div className="flex justify-center gap-6 px-4 py-3 rounded-t-3xl shadow">
       <Button
         size="icon"
-        className="w-12 h-12 rounded-full"
+        className="w-12 h-12 rounded-full flex-none"
         variant={"default"}
       >
         <CalendarIcon className="w-6 h-6" />
       </Button>
 
-      <Button size="icon" className="w-12 h-12 rounded-full" variant={"ghost"}>
+      <Button
+        size="icon"
+        className="w-12 h-12 rounded-full flex-none"
+        variant={"ghost"}
+      >
         <SearchIcon className="w-6 h-6" />
       </Button>
 
-      <Button size="icon" className="w-12 h-12 rounded-full" variant={"ghost"}>
+      <Button
+        size="icon"
+        className="w-12 h-12 rounded-full  flex-none"
+        variant={"ghost"}
+      >
         <HistoryIcon className="w-6 h-6" />
       </Button>
 
-      <Button size="icon" className="w-12 h-12 rounded-full" variant={"ghost"}>
+      <Button
+        size="icon"
+        className="w-12 h-12 rounded-full  flex-none"
+        variant={"ghost"}
+      >
         <HeartIcon className="w-6 h-6" />
       </Button>
 
-      <Button size="icon" className="w-12 h-12 rounded-full" variant={"ghost"}>
+      <Button
+        size="icon"
+        className="w-12 h-12 rounded-full  flex-none"
+        variant={"ghost"}
+      >
         <UserRoundIcon className="w-6 h-6" />
       </Button>
     </div>
